843b308dbe1b551c138e527e0d366b52385b31f1
Durdu-Mehmet-Karamanli <durdumehmetkaramanli@gmail.com>
2024-04-25 15:44:19 +0300

    sss

--- app.js
+++ app.js
@@ -25,19 +25,19 @@ var swiper = new Swiper(".mySwiper", {
   });
   var swiper3 = new Swiper(".mySwiper3", {
     slidesPerView: 1,
-    spaceBetween: 1,
+    spaceBetween: 2,
     pagination: {
       el: ".mySwiper3 .swiper-pagination",
       clickable: true,
     },
     breakpoints:{
       768:{
-        slidesPerView: 3,
+        slidesPerView: 2,
         spaceBetween: 20,
       },
       1024:{
-        slidesPerView: 5,
-        spaceBetween: 30,
+        slidesPerView: 2,
+        spaceBetween: 20,
       },
     }
   });
@@ -52,4 +52,21 @@ var swiper = new Swiper(".mySwiper", {
   $('.nav__menu li').click(function() {
     $('.nav__menu li').removeClass('active');
     $(this).addClass('active');
+  });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $('.nav__menu__active').removeClass('nav__menu__active');
+      $('.menu__active').removeClass('menu__active');
+
+    $('#hamb .first').removeClass('menu__first');
+    $('#hamb .second').removeClass('menu__second');
+    $('#hamb .third').removeClass('menu__third');
+    } else {
+      $('.nav__menu__active').toggleClass('nav__menu__active');
+      $('.menu__active').toggleClass('menu__active');
+
+    $('#hamb .first').toggleClass('menu__first');
+    $('#hamb .second').toggleClass('menu__second');
+    $('#hamb .third').toggleClass('menu__third');
+    }
   });
